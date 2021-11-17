9b0993ebf828f9c428c7410c6901ba3355a43bab
Ajinkya Naik <51918054+aj-naik@users.noreply.github.com>
2021-11-17 22:27:31 +0530

    Left side CSS Complete

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -4,35 +4,34 @@ import IngredientList from './IngredientList'
 export default function Recipe(props) {
     const {name, cookTime, servings, instructions, ingredients} = props
     return (
-        <div>
-           <div> 
-               <h3> {name} </h3>
+        <div className ="recipe">
+           <div className ="recipe__header"> 
+               <h3 className = "recipe__title"> {name} </h3>
             </div> 
             <div>
-                <button>Edit</button>
-                <button>Delete</button>
+                <button className = "btn btn--primary mr-1">Edit</button>
+                <button className = "btn btn--danger">Delete</button>
             </div>
-            <div>
-                <span>Cooking Time : </span>
-                <span>{cookTime}</span>
+            <div className = "recipe__row">
+                <span className = "recipe__label">Cooking Time : </span>
+                <span className = "recipe__value">{cookTime}</span>
             </div>
-            <div>
-                <span>Servings : </span>
-                <span>{servings}</span>
+            <div className = "recipe__row">
+                <span className = "recipe__label">Servings : </span>
+                <span className = "recipe__value">{servings}</span>
             </div>
-            <div>
-                <span>Instructions : </span>
-                <div>
+            <div className = "recipe__row">
+                <span className = "recipe__label">Ingredients : </span>
+                <div className = "recipe__value recipe__value--intended">
+                    <IngredientList ingredients={ingredients}/>
+                </div>
+            <div className = "recipe__row">
+                <span className = "recipe__label">Instructions : </span>
+                <div className = "recipe__value recipe__instructions recipe__value--intended">
                     {instructions}
                 </div>
             </div>
-            <div>
-                <span>Ingredients : </span>
-                <div>
-                    <IngredientList ingredients={ingredients}/>
-                </div>
             </div>
-            
         </div>
     )
 }
